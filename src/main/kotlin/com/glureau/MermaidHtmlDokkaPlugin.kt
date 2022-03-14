6d8d2e92f17540393e8fc072bdf98fe75fe1c994
Gregory Lureau <glureau@deezer.com>
2022-03-15 00:39:20 +0100

    Embed the mermaid css/js and install them via a PageTransformer.

--- src/main/kotlin/com/glureau/MermaidHtmlDokkaPlugin.kt
+++ src/main/kotlin/com/glureau/MermaidHtmlDokkaPlugin.kt
@@ -2,7 +2,12 @@ package com.glureau
 
 import org.jetbrains.dokka.CoreExtensions
 import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.pages.RendererSpecificResourcePage
+import org.jetbrains.dokka.pages.RenderingStrategy
+import org.jetbrains.dokka.pages.RootPageNode
+import org.jetbrains.dokka.plugability.DokkaContext
 import org.jetbrains.dokka.plugability.DokkaPlugin
+import org.jetbrains.dokka.transformers.pages.PageTransformer
 
 class MermaidHtmlDokkaPlugin : DokkaPlugin() {
 
@@ -12,7 +17,28 @@ class MermaidHtmlDokkaPlugin : DokkaPlugin() {
         CoreExtensions.renderer providing ::MermaidHtmlRenderer override dokkaBase.htmlRenderer
     }
 
+    val mermaidInstaller by extending {
+        dokkaBase.htmlPreprocessors providing ::MermaidInstaller order {
+            after(dokkaBase.assetsInstaller)
+            before(dokkaBase.customResourceInstaller)
+        }
+    }
+
     init {
         println("MermaidHtmlDokkaPlugin installed")
     }
+}
+
+class MermaidInstaller(private val dokkaContext: DokkaContext) : PageTransformer {
+    private val components = listOf(
+        "mermaid.css",
+        "mermaid.js",
+    )
+
+    override fun invoke(input: RootPageNode): RootPageNode = input
+        .modified(children = input.children + components.toRenderSpecificResourcePage())
+        .transformContentPagesTree { it.modified(embeddedResources = it.embeddedResources + components) }
+
+    private fun List<String>.toRenderSpecificResourcePage(): List<RendererSpecificResourcePage> =
+        map { RendererSpecificResourcePage(it, emptyList(), RenderingStrategy.Copy("/dokka/$it")) }
 }
---
+++ updateMermaid.sh
@@ -0,0 +1,3 @@
+curl https://cdnjs.cloudflare.com/ajax/libs/mermaid/$1/mermaid.css --output src/main/resources/dokka/mermaid.css
+curl https://cdnjs.cloudflare.com/ajax/libs/mermaid/$1/mermaid.css --output src/main/resources/dokka/mermaid.js
+echo "$1" > src/main/resources/dokka/mermaid_version.txt
