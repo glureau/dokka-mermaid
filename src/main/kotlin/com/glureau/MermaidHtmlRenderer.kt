c7db5116052500f4f25c6950ce8f9e1294218c83
Gregory Lureau <glureau@deezer.com>
2022-03-15 00:15:35 +0100

    Improve mermaid detection.

--- src/main/kotlin/com/glureau/MermaidHtmlRenderer.kt
+++ src/main/kotlin/com/glureau/MermaidHtmlRenderer.kt
@@ -15,6 +15,40 @@ open class MermaidHtmlRenderer(
     context: DokkaContext
 ) : HtmlRenderer(context) {
 
+    private val mermaidDetectionList = listOf(
+        // https://mermaid-js.github.io/mermaid/#/flowchart?id=flowchart-orientation
+        "flowchart TB",
+        "flowchart TD",
+        "flowchart BT",
+        "flowchart RL",
+        "flowchart LR",
+        // https://mermaid-js.github.io/mermaid/#/sequenceDiagram
+        "sequenceDiagram",
+        // https://mermaid-js.github.io/mermaid/#/classDiagram
+        "classDiagram",
+        // https://mermaid-js.github.io/mermaid/#/stateDiagram
+        "stateDiagram",
+        "stateDiagram-v2",
+        // https://mermaid-js.github.io/mermaid/#/entityRelationshipDiagram
+        "erDiagram",
+        // https://mermaid-js.github.io/mermaid/#/user-journey
+        "journey",
+        // https://mermaid-js.github.io/mermaid/#/gantt
+        "gantt",
+        // https://mermaid-js.github.io/mermaid/#/pie
+        "pie",
+        "pie title .*",
+        // https://mermaid-js.github.io/mermaid/#/requirementDiagram
+        "requirementDiagram",
+
+        // deduced from https://mermaid-js.github.io/mermaid/#/examples
+        "graph TB",
+        "graph TD",
+        "graph BT",
+        "graph RL",
+        "graph LR",
+    ).map { Regex(it) }
+
     override fun buildHtml(page: PageNode, resources: List<String>, content: FlowContent.() -> Unit): String {
         // TODO: Define a "Style"/script installer like here https://github.com/Kotlin/dokka/blob/c1efae49d5595c79ccbf004ceb6aabd7367ed9de/plugins/versioning/src/main/kotlin/versioning/htmlPreprocessors.kt
         val addedRes: List<String> = listOf(
@@ -26,12 +60,15 @@ open class MermaidHtmlRenderer(
     }
 
     override fun FlowContent.buildCodeBlock(code: ContentCodeBlock, pageContext: ContentPage) {
-        // TODO: List all Mermaid combinations
         // TODO: Understand why language is not filled with "mermaid" in the unit test
-        val guessMermaid: Boolean =
-            (code.language == "") && ((code.children.firstOrNull() as? ContentText)?.text?.startsWith("graph ") == true)
-
-        if (code.language == "mermaid" || guessMermaid) {
+        var isMermaidGraph = code.language == "mermaid"
+        if (!isMermaidGraph && code.language == "") { // Trying to guess if it's actually a Mermaid graph
+            val firstLine = (code.children.firstOrNull() as? ContentText)?.text?.trim()
+            if (firstLine != null) {
+                isMermaidGraph = mermaidDetectionList.any { it.matches(firstLine) }
+            }
+        }
+        if (isMermaidGraph) {
             div("sample-container") {
                 div("mermaid") {
                     +code.children.filterIsInstance<ContentText>().joinToString("\n") { it.text }
--- src/test/kotlin/template/MermaidHtmlDokkaPluginTest.kt
+++ src/test/kotlin/template/MermaidHtmlDokkaPluginTest.kt
@@ -1,8 +1,8 @@
 package template
 
-import junit.framework.Assert.assertNotNull
 import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class MermaidHtmlDokkaPluginTest : BaseAbstractTest() {
     private val configuration = dokkaConfiguration {
@@ -14,14 +14,14 @@ class MermaidHtmlDokkaPluginTest : BaseAbstractTest() {
     }
 
     @Test
-    fun `my awesome plugin should find packages and classes`() {
+    fun `my plugin should render class code blocks containing mermaid code`() {
         testInline(
             """
             |/src/main/kotlin/sample/Test.kt
             |package sample
             | /**
             |  * A visual illustration brought by Mermaid:
-            |  * ``` mermaid
+            |  * ```mermaid
             |  * graph TD
             |  *   A[Christmas] -->|Get money| B(Go shopping)
             |  *   B --> C{Let me think}
@@ -35,13 +35,11 @@ class MermaidHtmlDokkaPluginTest : BaseAbstractTest() {
             cleanupOutput = false
         ) {
             documentablesTransformationStage = { module ->
-                val testedPackage = module.packages.find { it.name == "sample" }
-                val testedClass = testedPackage?.classlikes?.find { it.name == "TestingIsEasy" }
-
-                assertNotNull(testedPackage)
-                assertNotNull(testedClass)
-
-                // TODO: Proper testing...
+                val testedPackage = module.packages.find { it.name == "sample" }!!
+                val testedClass = testedPackage.classlikes.find { it.name == "ChristmasStateMachine" }!!
+                val codeBlock = testedClass.documentation.values.first().children.first().children[1]
+                assertEquals("mermaid", codeBlock.params["lang"])
+                // Here I can check more about the structure but no idea where to check the generated html.
             }
         }
     }
